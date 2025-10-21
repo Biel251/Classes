493e80d132921e4ae42cba15b75fb2be4ace78a9
Biel251 <joaogabriel20042007@gmail.com>
2025-10-21 20:01:36 -0300

    Update carro.js

---
+++ carro.js
@@ -0,0 +1,30 @@
+class Carro {
+  constructor(modelo) {
+    this.modelo = modelo;
+    this.velocidade = 0;
+  }
+
+  acelerar() {
+    this.velocidade += 10;
+  }
+
+  frear() {
+    if (this.velocidade >= 10) {
+      this.velocidade -= 10;
+    } else {
+      this.velocidade = 0;
+    }
+  }
+
+  mostrarVelocidade() {
+    console.log(`A velocidade atual do ${this.modelo} é ${this.velocidade} km/h`);
+  }
+}
+
+// Exemplo de uso
+const carro1 = new Carro("Fusca");
+carro1.acelerar();
+carro1.acelerar();
+carro1.mostrarVelocidade();
+carro1.frear();
+carro1.mostrarVelocidade();
